d5ba268a603b4d53e095c90798bebd846b47e303
Edgar <edgar7sagan@hotmail.com>
2019-12-16 02:36:31 +0700

    Update main.cpp
    modified insert

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,18 @@
 
 int main(){
 	    // insert code here...
-    std::cout << "HashMap example";
-    hashmap<std::string,int> hashy;
-    hashy.insert("juan", 34);
-    hashy.insert("lera", 22);
-    hashy.insert("vera", 24);
-    hashy.insert("vlad", 23);
-    hashy.insert("edgar", 29);
-    hashy.insert("paul", 43);
-    hashy.insert("paula", 78);
-    hashy.insert("roman", 14);
-    hashy.insert("mereke", 37);
-    hashy.insert("natalya", 25);
-    hashy.insert("aysuluu", 20);
+     hashmap<std::string,int> hashy;
+    hashy.insert(std::pair<std::string, int>("juan", 34));
+    hashy.insert(std::pair<std::string, int>("lera", 22));
+    hashy.insert(std::pair<std::string, int>("vera", 24));
+    hashy.insert(std::pair<std::string, int>("vlad", 23));
+    hashy.insert(std::pair<std::string, int>("edgar", 29));
+    hashy.insert(std::pair<std::string, int>("paul", 43));
+    hashy.insert(std::pair<std::string, int>("paula", 78));
+    hashy.insert(std::pair<std::string, int>("roman", 14));
+    hashy.insert(std::pair<std::string, int>("mereke", 37));
+    hashy.insert(std::pair<std::string, int>("natalya", 25));
+    hashy.insert(std::pair<std::string, int>("aysuluu", 20));
     hashy.printMap();
     std::cout << "the number of elements in the first bucket is " << hashy.count(1) << " \n";
     hashy.bucketContent(1);
@@ -33,13 +32,14 @@ int main(){
         }else{
             break;
         }
-
     }
     hashy.bucketContent(0);
     hashy.printMap();
     hashy.at("mereke");
     hashy.at("michael");
-    hashy.iteratorbegin(34);
+    hashy.begin();
+    hashy.end();
     std::cout<<"example using the iterator to get a key, key from value 34: "<<hashy.iteratorbegin(34)->key<< " \n";
+    
     return 0;
 }
